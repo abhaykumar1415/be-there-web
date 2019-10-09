d070ff2a9b2b9ae578238a0d72da8d309eb840ef
Abhay Kumar <abhay.kumar@qed42.com>
2019-10-09 20:55:13 +0530

    pointing baseUrl to prod

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,7 +1,7 @@
 const URL = Object.freeze({
-  BASE_URL : 'http://localhost:4000/api/',
+  // BASE_URL : 'http://localhost:4000/api/',
   // BASE_URL : 'http://13.250.14.150:4000/api/',
-  // BASE_URL : 'https://qed42.herokuapp.com/api/',
+  BASE_URL : 'https://qed42.herokuapp.com/api/',
   USERS: "users/",
   ME_TODAY :"meToday/",
   SECRET_KEY: "EEA1C75D7FDCD69BA9DD1612DA4B9"
